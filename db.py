e7d24a3d2503c2b8b38a5f84a81b86e721d7e038
Natan <natan@hustin.be>
2026-06-16 11:07:38 +0200

    added db.py and session

--- db.py
+++ db.py
@@ -1,2 +1,12 @@
 from sqlalchemy import create_engine
-from models.py import
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import Base
+
+
+engine = create_engine("postgresql://postegres:Root@localhost:5432/postgres")
+
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+
