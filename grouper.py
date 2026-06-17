5ebff70380db6519d4b9692189bdec56ad5d6bff
Natan Hustin <natan@hustin.be>
2026-06-17 22:23:39 +0200

    tried something, failed badly, went to sleep

--- grouper.py
+++ grouper.py
@@ -1,6 +1,6 @@
 from db import Session
 from models import GeoZone, Orders, OrderLines,Pallets,PalletLines,Shipments,ShipmentPallet
-
+from datetime import datetime
 
 
 def get_pending_orders(session):
@@ -9,8 +9,19 @@ def get_pending_orders(session):
 
 def group_orders(session):
     orders = session.query(Orders).filter(Orders.status.in_(['validated', 'ready'])).order_by(Orders.geo_zone_id, Orders.status, Orders.eta).all()
+    groups = list()
     for order in orders:
-        print (order)
+        if groups == []:
+                eta_ref = order.eta.replace(hour=8, minute=0, second=0, microsecond=0)
+                groups.append({'geo_zone_id':order.geo_zone_id,'eta_ref': eta_ref, 'orders': [order]})
+        else:
+            if order.status == 'ready':
+            
+                for group in groups:
+                
+
+           
+    
 
 
 def apply_grouping(session, groups):
