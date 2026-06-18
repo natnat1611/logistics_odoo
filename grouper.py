a3262141812a4bcb7cc976223d94d09dacef003a
Natan <natan@hustin.be>
2026-06-18 13:17:32 +0200

    grouping engine done

--- grouper.py
+++ grouper.py
@@ -7,19 +7,37 @@ def get_pending_orders(session):
     pending = session.query(Orders).filter(Orders.status.in_(['validated', 'ready'])).all()
     return pending
 
-def group_orders(session):
-    orders = session.query(Orders).filter(Orders.status.in_(['validated', 'ready'])).order_by(Orders.geo_zone_id, Orders.status, Orders.eta).all()
-    groups = list()
-    for order in orders:
-        if groups == []:
-                eta_ref = order.eta.replace(hour=8, minute=0, second=0, microsecond=0)
-                groups.append({'geo_zone_id':order.geo_zone_id,'eta_ref': eta_ref, 'orders': [order]})
-        else:
-            if order.status == 'ready':
-            
-                for group in groups:
-                
 
+def group_orders(session, window_hours=48):
+    orders = session.query(Orders)\
+        .filter(Orders.status.in_(['validated', 'ready']))\
+        .order_by(Orders.geo_zone_id, Orders.status, Orders.eta)\
+        .all()
+    
+    groups = []
+    
+    for order in orders:
+        if order.status == 'ready':
+            for group in groups:
+                if group['geo_zone_id'] == order.geo_zone_id:
+                    group['orders'].append(order)
+                    break
+            else:
+                eta_ref = order.eta.replace(hour=8, minute=0, second=0, microsecond=0)
+                groups.append({'geo_zone_id': order.geo_zone_id, 'eta_ref': eta_ref, 'orders': [order]})
+        
+        else:  # validated
+            for group in groups:
+                if group['geo_zone_id'] == order.geo_zone_id:
+                    ecart = abs((order.eta - group['eta_ref']).total_seconds()) / 3600
+                    if ecart <= window_hours:
+                        group['orders'].append(order)
+                        break
+            else:
+                eta_ref = order.eta.replace(hour=8, minute=0, second=0, microsecond=0)
+                groups.append({'geo_zone_id': order.geo_zone_id, 'eta_ref': eta_ref, 'orders': [order]})
+    
+    return groups
            
     
 
@@ -38,4 +56,7 @@ def run_grouping(session, window_hours=48):
 
 
 if __name__ == '__main__':
-    group_orders(Session)
+    groups =  group_orders(Session)
+
+    for group in groups:
+        print(f"Zone {group['geo_zone_id']} - ETA ref {group['eta_ref']} - {len(group['orders'])} orders")
